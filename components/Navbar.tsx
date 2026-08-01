caddf97526e6a844a51ba5a8004630d854b39412
mukitalam <mukitalam9@gmail.com>
2026-08-01 13:01:09 +0600

    first commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -329,7 +329,6 @@ export default function Navbar() {
                 text-white/60
               "
             >
-              Diplomacy • Leadership
             </p>
           </div>
         </Link>
